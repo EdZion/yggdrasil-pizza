37ce526094f8b882dd9622679e39b749a69263dc
alluns <allan.backman@gmail.com>
2021-10-18 12:52:15 +0200

    🗃️ more pizza

--- src/_data/pizza.js
+++ src/_data/pizza.js
@@ -1,4 +1,4 @@
-const toppings = ['Tomatsås', 'Ost', 'Skinka', 'Champinjoner', 'Ananas', 'Räkor', 'Musslor', 'Krabba', 'Prosciutto', 'Parma Skinka', 'Pancetta'];
+const toppings = ['Tomatsås', ' Ost', ' Skinka', ' Champinjoner', ' Ananas', ' Räkor', ' Musslor', ' Krabba', ' Prosciutto', ' Parma Skinka', ' Pancetta', ' Kyckling', ' Salami', ' Salad', ' Lök', ' Gurka', ' Tomat', ' Peperoni', ' Kebab', ' Kebabsås'];
 const category = ['Klassiker', 'Inbakad', 'Rullpizza', 'Kebab'];
 
 module.exports = () => {
@@ -60,6 +60,34 @@ module.exports = () => {
         category: category[1],
         price: 137,
         toppings: [toppings[0], toppings[1], toppings[2], toppings[8], toppings[9], toppings[10]],
+      },
+      {
+        id: 9,
+        name: 'Alexandra Special',
+        category: category[2],
+        price: 360,
+        toppings: [toppings[0], toppings[1], toppings[18], toppings[19], toppings[15], toppings[13], toppings[12], toppings[11], toppings[4]],
+      },
+      {
+        id: 10,
+        name: 'Rullzone',
+        category: category[2],
+        price: 85,
+        toppings: [toppings[0], toppings[1], toppings[2]],
+      },
+      {
+        id: 11,
+        name: 'Kebaq pizza',
+        category: category[3],
+        price: 90,
+        toppings: [toppings[0], toppings[1], toppings[18], toppings[19]],
+      },
+      {
+        id: 12,
+        name: 'Allan Special',
+        category: category[3],
+        price: 100,
+        toppings: [toppings[0], toppings[1], toppings[18], toppings[19], ' kärlek'],
       },
     ],
   };
