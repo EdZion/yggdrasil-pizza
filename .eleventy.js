8b9b06fa0c6675273e704f02f94a17bbc6cc8726
edzion <alle020916@gmail.com>
2021-10-19 14:19:36 +0200

    🍱 Fixed fonts on mobile

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const svgSprite = require("eleventy-plugin-svg-sprite");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/fonts/norse");
   return {
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
